61f7984edac8357018783cb13eb9f36f87aade12
陈钦辉 <chenqinhui@didichuxing.com>
2017-11-02 15:11:16 +0800

    @dev update

--- main.js
+++ main.js
@@ -1,141 +1,165 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   var allEvents = $('#allEvents'),
-     divDebounce_true = $('#debounce_true'),
-     divDebouncejtrue = $('#debouncejtrue'),
-     divDebounce_false = $('#debounce_false'),
-     divDebouncejfalse = $('#debouncejfalse'),
-     divThrottle_true = $('#throttle_true'),
-     divThrottlejtrue = $('#throttlejtrue'),
-     divThrottlejfalse = $('#throttlejfalse'),
-     sidebar_mousemove = $('#sidebar-free'),
-     counter = 0,
-     next_color = 0,
-     drawing,
-     drawing_automated,
-     lazy_Debounce_Events,
-     lazyDebounce_true,
-     lazyDebouncejtrue,
-     lazyDebounce_false,
-     lazyDebouncejfalse,
-     lazyThrottle_true,
-     lazyThrottlejtrue,
-     lazyThrottlejfalse;
 
+    divDebounce_false_true = $('#debounce_false_true'),
+    divDebounce_true_false = $('#debounce_true_false'),
+    divDebounce_false_false = $('#debounce_false_false'),
+    divDebounce_true_true = $('#debounce_true_true'),
 
-  function update(div, color){
-    div[0].lastChild.className = 'color' + color;
-    div[0].lastChild.innerHTML = color;
+    divThrottle_true_true = $('#throttle_true_true'),
+    divThrottle_false_false = $('#throttle_false_false'),
+    divThrottle_true_false = $('#throttle_true_false'),
+    divThrottle_false_true = $('#throttle_false_true'),
+
+    sidebar_mousemove = $('#sidebar-free'),
+
+    counter = 0,
+    next_color = 0,
+    drawing,
+    drawing_automated,
+    lazy_Debounce_Events,
+
+    lazyDebounce_false_true,
+    lazyDebounce_true_false,
+    lazyDebounce_false_false,
+    lazyDebounce_true_true,
+
+    lazyThrottle_true_true,
+    lazyThrottle_false_false,
+    lazyThrottle_true_false,
+    lazyThrottle_false_true;
+
+  function update (div, color) {
+    div[0].lastChild.className = 'color' + color
+    div[0].lastChild.innerHTML = color
   }
 
+  function setup_lazy_functions (_) {
 
-  function setup_lazy_functions(_){
-    lazy_Debounce_Events = $.throttle(50, false, updateEvents);
+    lazy_Debounce_Events = _.throttle(updateEvents, 50, {
+      leading: true,
+      trailing: true
+    });
 
-    lazyDebounce_true = _.debounce(update, 200, true);
-    lazyDebouncejtrue = $.debounce(200, true, update);
 
-    lazyDebounce_false = _.debounce(update, 200, false);
-    lazyDebouncejfalse = $.debounce(200, false, update);
+    // 默认
+    lazyDebounce_false_true = _.debounce(update, 200, {
+      leading: false,
+      trailing: true
+    })
 
-    lazyThrottle_true = _.throttle(update, 200);
-    lazyThrottlejtrue = $.throttle(200, true, update);
-    lazyThrottlejfalse = $.throttle(200, false, update);
+    lazyDebounce_true_false = _.debounce(update, 200, {
+      leading: true,
+      trailing: false
+    })
+
+    lazyDebounce_true_true = _.debounce(update, 200, {
+      leading: true,
+      trailing: true
+    })
+
+    lazyDebounce_false_false = _.debounce(update, 200, {
+      leading: true,
+      trailing: true
+    })
+
+
+
+
+    lazyThrottle_true_true = _.throttle(update, 200, {
+      leading: true,
+      trailing: true
+    })
+
+    lazyThrottle_false_false = _.throttle(update, 200, {
+      leading: false,
+      trailing: false
+    })
+
+    lazyThrottle_true_false = _.throttle(update, 200, {
+      leading: true,
+      trailing: false
+    })
+
+    lazyThrottle_false_true = _.throttle(update, 200, {
+      leading: false,
+      trailing: true
+    })
   }
 
-  function updateEvents(){
-    update(allEvents, next_color);
-    lazyDebounce_true(divDebounce_true, next_color);
-    lazyDebouncejtrue(divDebouncejtrue, next_color);
-    lazyDebounce_false(divDebounce_false, next_color);
-    lazyDebouncejfalse(divDebouncejfalse, next_color);
-    lazyThrottle_true(divThrottle_true, next_color);
-    lazyThrottlejtrue(divThrottlejtrue, next_color);
-    lazyThrottlejfalse(divThrottlejfalse, next_color);
-    next_color++;
-    if (next_color > 9){
-      next_color = 0;
+  function updateEvents () {
+    update(allEvents, next_color)
+    lazyDebounce_false_true(divDebounce_false_true, next_color)
+    lazyDebounce_true_false(divDebounce_true_false, next_color)
+    lazyDebounce_false_false(divDebounce_false_false, next_color)
+    lazyDebounce_true_true(divDebounce_true_true, next_color)
+
+    lazyThrottle_false_true(divThrottle_false_true, next_color)
+    lazyThrottle_true_false(divThrottle_true_false, next_color)
+    lazyThrottle_false_false(divThrottle_false_false, next_color)
+    lazyThrottle_true_true(divThrottle_true_true, next_color)
+
+    next_color++
+    if (next_color > 9) {
+      next_color = 0
     }
   }
 
-  // Initially demo it with underscore.js
-  setup_lazy_functions(_);
+  setup_lazy_functions(_)
 
-  function reset(){
-    allEvents.html('<span></span>');
-    divDebounce_true.html('<span></span>');
-    divDebouncejtrue.html('<span></span>');
-    divDebounce_false.html('<span></span>');
-    divDebouncejfalse.html('<span></span>');
-    divThrottle_true.html('<span></span>');
-    divThrottlejtrue.html('<span></span>');
-    divThrottlejfalse.html('<span></span>');
-    next_color = 0;
-    counter = 0;
-    clearInterval(drawing_automated);
-    clearInterval(drawing);
+  function reset () {
+    allEvents.html('<span></span>')
+    divDebounce_false_true.html('<span></span>')
+    divDebounce_true_false.html('<span></span>')
+    divDebounce_false_false.html('<span></span>')
+    divDebounce_true_true.html('<span></span>')
+
+    divThrottle_false_true.html('<span></span>')
+    divThrottle_true_false.html('<span></span>')
+    divThrottle_false_false.html('<span></span>')
+    divThrottle_true_true.html('<span></span>')
+    next_color = 0
+    counter = 0
+    clearInterval(drawing_automated)
+    clearInterval(drawing)
   }
 
-  sidebar_mousemove.on('mousemove', function (){
-    lazy_Debounce_Events();
-  });
+  sidebar_mousemove.on('mousemove', function () {
+    lazy_Debounce_Events()
+  })
 
-  sidebar_mousemove.on('mouseenter', function(){
-    reset();
-    draw();
-  });
+  sidebar_mousemove.on('mouseenter', function () {
+    reset()
+    draw()
+  })
 
-  $('#every100').on('click', function(e){
-    e.preventDefault();
-    reset();
-    draw();
-    drawing_automated = setInterval(function(){
-      sidebar_mousemove.trigger('mousemove');
-    }, 100);
-  });
+  var draw = function () {
+    drawing = setInterval(function () {
+      counter++
+      allEvents[0].appendChild(document.createElement('span'))
 
+      divDebounce_false_true[0].appendChild(document.createElement('span'))
+      divDebounce_true_false[0].appendChild(document.createElement('span'))
+      divDebounce_false_false[0].appendChild(document.createElement('span'))
+      divDebounce_true_true[0].appendChild(document.createElement('span'))
 
-  $('#every300').on('click', function(e){
-    e.preventDefault();
-    reset();
-    draw();
-    drawing_automated = setInterval(function(){
-      sidebar_mousemove.trigger('mousemove');
-      sidebar_mousemove.trigger('mousemove');
-    }, 300);
-   });
+      divThrottle_false_true[0].appendChild(document.createElement('span'))
+      divThrottle_true_false[0].appendChild(document.createElement('span'))
+      divThrottle_false_false[0].appendChild(document.createElement('span'))
+      divThrottle_true_true[0].appendChild(document.createElement('span'))
 
-
-  $('#use-lodash').on('click', function(e){
-    e.preventDefault();
-    if ($(this).data('lodash')){
-      setup_lazy_functions(_);
-      $(this).data('lodash', false)
-             .html('Using <strong>underscore.js</strong> | <del>lodash.js</del>')
-    } else {
-      setup_lazy_functions(lo);
-      $(this).data('lodash', true)
-             .html('Using <del>underscore.js</del> | <strong>lodash.js</strong>')
-    }
-  });
-
-  var draw = function(){
-    drawing = setInterval(function(){
-      counter++;
-      allEvents[0].appendChild(document.createElement('span'));
-      divDebounce_true[0].appendChild(document.createElement('span'));
-      divDebouncejtrue[0].appendChild(document.createElement('span'));
-      divDebounce_false[0].appendChild(document.createElement('span'));
-      divDebouncejfalse[0].appendChild(document.createElement('span'));
-      divThrottlejtrue[0].appendChild(document.createElement('span'));
-      divThrottlejfalse[0].appendChild(document.createElement('span'));
-      divThrottle_true[0].appendChild(document.createElement('span'));
-
-      if (counter > 95){
-        clearInterval(drawing);
-        clearInterval(drawing_automated);
+      if (counter > 95) {
+        clearInterval(drawing)
+        clearInterval(drawing_automated)
       }
-    }, 30);
-  };
+    }, 30)
+  }
 
-});
+  reset();
+  draw();
+  drawing_automated = setInterval(function(){
+    sidebar_mousemove.trigger('mousemove');
+  }, 100);
+
+})
